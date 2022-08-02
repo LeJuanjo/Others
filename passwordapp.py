708cc2f97442e3cc0948865bb8425cf21ef828b6
LeJuanjo <93773370+LeJuanjo@users.noreply.github.com>
2022-08-02 19:47:33 +0200

    Add files via upload

--- passwordapp.py
+++ passwordapp.py
@@ -1,5 +1,5 @@
-import streamlit as st
 
+import streamlit as st
 # Everything is accessible via the st.secrets dict:
 
 st.write("DB username:", st.secrets["db_username"])
@@ -14,3 +14,42 @@ st.write(
     "Has environment variables been set:",
     os.environ["db_username"] == st.secrets["db_username"],
 )
+
+
+
+
+# streamlit_app.py
+
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
