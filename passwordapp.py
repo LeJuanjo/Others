88cde76bf88b5089af6efa5d0655f1300434d091
LeJuanjo <93773370+LeJuanjo@users.noreply.github.com>
2022-08-02 19:55:13 +0200

    Add files via upload

--- passwordapp.py
+++ passwordapp.py
@@ -2,18 +2,7 @@
 import streamlit as st
 # Everything is accessible via the st.secrets dict:
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
-# And the root-level secrets are also accessible as environment variables:
-
-import os
-
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
 
 
 
@@ -27,7 +16,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
+        if st.session_state["password"] == st.secrets["db_password"]:
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store password
         else:
